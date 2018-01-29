1092c38b7e866c26b650af1a50bd66a4841fc879
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-01-30 00:00:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
+#./cpuminer -t 60 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
