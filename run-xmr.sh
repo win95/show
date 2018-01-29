1720ae5312942242621937a70a11caf7b616abbc
Your Name <you@example.com>
2018-01-29 15:51:43 +0700

    first commit

---
+++ run-xmr.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install -y libcurl4-openssl-dev git build-essential tmux
+apt-get install -y autotools-dev autoconf libcurl3 libcurl4-gnutls-dev
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+make install
---
+++ start.sh
@@ -0,0 +1,1 @@
+minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
