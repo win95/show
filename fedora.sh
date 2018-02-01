a4172aada37d063ed93f4a3880c6a41fc33dfa43
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-02-01 23:34:31 +0700

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,7 @@
+yum -y install git curl-devel libcurl glib-devel libtool
+git clone https://github.com/hyc/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+./minerd -a cryptonight
