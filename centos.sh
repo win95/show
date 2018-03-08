6022adff26befbdd8d2cbf7921c5664a5f950fa3
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-03-08 02:26:55 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,5 +1,5 @@
-yum groupinstall "Development Tools"
-yum install curl-devel openssl-devel git screen
+yum groupinstall "Development Tools" -y
+yum install curl-devel openssl-devel git screen -y
 git clone https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
 ./autogen.sh
