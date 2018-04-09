76f9d303a01ec8f8368ac680d6b520aa70775072
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-04-09 10:06:11 +0700

    Update start.sh
    update port xmr

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
-#./cpuminer -t 60 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
+minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u ductran9xhp@gmail.com -p x
+#./cpuminer -t 60 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u ductran9xhp@gmail.com -p x
