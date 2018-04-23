5040f0ebdecf9e48e50e15e45e4c5903e1bb683d
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-04-23 17:08:53 +0700

    Update xmrig-centos.sh

--- xmrig-centos.sh
+++ xmrig-centos.sh
@@ -1,7 +1,7 @@
 yum install -y epel-release
 yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+git clone https://github.com/win95/xmr-miner.git
+cd xmr-miner
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
