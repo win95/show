be19e60b9b56f686fdcddc42f8eb205bc1909bf6
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-04-23 17:08:19 +0700

    Update xmrig-ubuntu-gcc7.sh

--- xmrig-ubuntu-gcc7.sh
+++ xmrig-ubuntu-gcc7.sh
@@ -4,8 +4,8 @@ add-apt-repository ppa:jonathonf/gcc-7.1
 apt-get update
 apt-get install gcc-7 g++-7
 
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+git clone https://github.com/win95/xmr-miner.git
+cd xmr-miner
 mkdir build
 cd build
 cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
