b01c5bc5665823722afb1a61c1c066d1dea4dbb8
ductn-viosoft <30426640+ductn-viosoft@users.noreply.github.com>
2018-04-23 17:07:17 +0700

    Update xmrig-ubuntu.sh

--- xmrig-ubuntu.sh
+++ xmrig-ubuntu.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev tmux screen
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+https://github.com/win95/xmr-miner.git
+cd xmr-miner
 mkdir build
 cd build
 cmake ..
